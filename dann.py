8a767cd58620caf3fea1789328e5487364cc8b84
Bujiazi <3351506128@qq.com>
2023-12-21 20:40:30 +0800

    dann finished

--- dann.py
+++ dann.py
@@ -12,9 +12,11 @@ from utils.utils import set_all_seeds
 
 @torch.no_grad()
 def test(dataloader, dataset, epoch):
-    model = torch.load(f"./save/other_methods/mnist_mnistm_model_epoch_{epoch}.pth")
-    model = model.eval()
+    ckpt = torch.load(f"./save/other_methods/mymodel_dann_epoch_{epoch}.pt")
+    model = MyModel_dann()
+    model.load_state_dict(ckpt)
     model = model.to('cuda')
+    model = model.eval()
 
     correct = 0
     total = 0
@@ -39,30 +41,29 @@ model.to('cuda')
 
 criterion_cls = nn.CrossEntropyLoss()
 criterion_dom = nn.CrossEntropyLoss()
-
 optimizer = optim.Adam(model.parameters(), lr=0.001)
     
-img_transform_source = transforms.Compose([
+img_source_transform = transforms.Compose([
     transforms.Resize(28),
     transforms.ToTensor(),
     transforms.Lambda(lambda x: torch.cat([x, x, x])),
     transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))
 ])
 
-target_transform_source = transforms.Compose([
+target_source_transform = transforms.Compose([
     transforms.Lambda(lambda x: 0 if x < 5 else 1)
 ])
 
-img_transform_target = transforms.Compose([
+img_target_transform = transforms.Compose([
     transforms.Resize(28),
     transforms.ToTensor(),
     transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))
 ])
 
-dataset_source_train = datasets.MNIST(root='../', train=True, transform=img_transform_source, target_transform=target_transform_source, download=False)
-dataset_source_test = datasets.MNIST(root='../', train=False, transform=img_transform_source, target_transform=target_transform_source, download=False)
-dataset_target_train = ColoredMNIST(transform=img_transform_target, env='train1')
-dataset_target_test = ColoredMNIST(transform=img_transform_target, env='test')
+dataset_source_train = datasets.MNIST(root='../', train=True, transform=img_source_transform, target_transform=target_source_transform, download=False)
+dataset_source_test = datasets.MNIST(root='../', train=False, transform=img_source_transform, target_transform=target_source_transform, download=False)
+dataset_target_train = ColoredMNIST(transform=img_target_transform, env='train1')
+dataset_target_test = ColoredMNIST(transform=img_target_transform, env='test')
 
 dataloader_source_train = DataLoader(dataset_source_train, batch_size=32, shuffle=True)
 dataloader_source_test = DataLoader(dataset_source_test, batch_size=32, shuffle=False)
@@ -81,8 +82,8 @@ for epoch in range(num_epochs):
         
         optimizer.zero_grad()
         
-        p = float(idx + epoch * min_len) / num_epochs / min_len
-        alpha = 2. / (1. + np.exp(-10 * p)) - 1
+        p = float(idx + min_len * epoch) / (num_epochs * min_len)
+        alpha = 2.0 / (1.0 + np.exp(-10 * p)) - 1
 
         domain_label_s = torch.zeros(len(target_s)).long()
         domain_label_t = torch.ones(len(target_t)).long()
@@ -105,7 +106,7 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
             
-    torch.save(model, f'./save/other_methods/mnist_mnistm_model_epoch_{epoch}.pth')
+    torch.save(model.state_dict(), f'./save/other_methods/mymodel_dann_epoch_{epoch}.pt')
     
     acc_s = test(dataloader_source_test, 'source', epoch)
     acc_t = test(dataloader_target_test, 'target', epoch)
